36b68bad2f4802d98dc1eeadbc5daf72df69c2b8
MarcNi02 <ubufi@student.kit.edu>
2020-03-01 17:39:47 +0100

    commit for push

--- app2.js
+++ app2.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     M.updateTextFields();
 
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    $("html, body").css({"width":w,"height":h});
+
     $('.fixed-action-btn').floatingActionButton();
 });
 
